1505d77b6f6eae53ef82a5674552f043c57dc700
Evgeniy <evgeniipusser88@gmail.com>
2025-02-24 21:14:50 +0200

    with one cycle

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function minMax(arr) {
-  let min = myReduce(arr, (acc, val) => (val < acc ? val : acc), arr[0]);
-  let max = myReduce(arr, (acc, val) => (val > acc ? val : acc), arr[0]);
-  return [min, max];
+  return myReduce(
+    arr,
+    (acc, val) => {
+      return [val < acc[0] ? val : acc[0], val > acc[1] ? val : acc[1]];
+    },
+    [arr[0], arr[0]]
+  );
 }
 
 function myReduce(array, callback, initialValue) {
@@ -17,13 +21,21 @@ function myReduce(array, callback, initialValue) {
 
 const strings = ["uiui", "Jio", "000", "Triangle", "MORNING", ""];
 const numbers = [-90, 0, 89.89, 2300];
-console.log(`Original Array: ${[-90, 0, 89.89, 2300]} gives min and max as ${minMax([-90, 0, 89.89, 2300])} minMax[0]===-90 minMax[1]===2300}`);
-console.log(`Original Array: ${strings} gives min and max as ${minMax(strings)} minMax[0]==="" minMax[1]==="uiui"`);
+console.log(
+  `Original Array: ${[-90, 0, 89.89, 2300]} gives min and max as ${minMax([
+    -90, 0, 89.89, 2300,
+  ])} minMax[0]===-90 minMax[1]===2300}`
+);
+console.log(
+  `Original Array: ${strings} gives min and max as ${minMax(
+    strings
+  )} minMax[0]==="" minMax[1]==="uiui"`
+);
 
-console.log(myReduce(numbers, (acc, val) => acc + val, 0)); 
+console.log(myReduce(numbers, (acc, val) => acc + val, 0));
 // Output: 27 (sum of all elements)
 
-console.log(minMax(numbers)); 
+console.log(minMax(numbers));
 // Output: [1, 9] (min and max values)
 
 console.log(minMax(strings));
